d5dc83a03d46d949a1a9fd5d159f2b6e0d699706
JPickard1 <jpic@umich.edu>
2023-09-18 21:58:57 -0400

    Kalman filtering in progress..its gonna be a slow burn

--- matlab/notebooks/sensor_selection.m
+++ matlab/notebooks/sensor_selection.m
@@ -41,3 +41,53 @@ clearvars -except D genes                           % Remove all other variables
 % Perform sensor selection
 [S, GV] = hasnain2023(D);
 genes.GeneName(S)
+
+%% Kalman Filtering
+%
+%   In this section I construct Kalman filters to estimate the full state
+%   vector of gene expression. We evaluate the performance of each gene set
+%   according to the convergence of each estimator.
+
+clear
+[D, genes] = loadMYOD(); replicates = 3; t = 15;
+[D, genes] = loadHasnain2023(); replicates = 2; t = 9;
+n = size(D,1);
+
+output = shiftedDMD(D,replicates,[],0.9);
+
+A_bar = output.Xp*output.DMD.VX*pinv(output.DMD.Sig)*output.DMD.UX';
+
+% Perform sensor selection
+[S, GV] = hasnain2023(D);
+C = getC(S, n); C = full(C);
+m = size(C,1);
+
+Y = C * D; Y = Y(:,1:t);
+
+% Vd = 1e-3 * ones(n,n); % process covariance
+% Vn = 1e-3 * ones(m,m); % sensor covariance
+% Vd = corr(D',D');
+% Vn = corr(Y',Y');
+Vd = 1e-1 * eye(n,n);
+Vn = 1e-1 * eye(m,m);
+B = zeros(n,1);
+[L, ~, ~] = lqe(A_bar, Vd, C, Vd, Vn);
+
+% Kf = lqr(A',C',Vd,Vn)';
+sysKF = ss(A - L*C, [B L], eye(n,n), 0 * [B L]);
+
+
+
+
+
+% Construct the system and filter
+Ts = -1;    % Discrete system
+
+sys = ss(A_bar,zeros(n,1),C,zeros(m,1),Ts,'OutputName','y');    % Construct system
+
+Q = 0;    % process noise // We will need to determine how to estimate these values
+R = 0;      % sensor noise
+
+[kalmf,L,~,Mx,Z] = kalman(sys,Q,R); % Construct Kalman Filter
+
+Xhat = lsim(kalmf, Ydata, t=1:9)
