229cd595a65cc014cc1563b2a5bb1341cdd74299
Joshua Pickard <jpic@umich.edu>
2023-09-20 14:13:12 -0400

    still doesnt work

--- matlab/notebooks/sensor_selection.m
+++ matlab/notebooks/sensor_selection.m
@@ -116,6 +116,53 @@ C = [1 0 0];
 
 D = 0;
 
+Ts = -1;
+sys = ss(A,[B B],C,D,Ts,'InputName',{'u' 'w'},'OutputName','y');  % Plant dynamics and additive input noise w
+
+Q = 2.3; 
+R = 1; 
+
+[kalmf,L,~,Mx,Z] = kalman(sys,Q,R);
+
+sys.InputName = {'u','w'};
+sys.OutputName = {'yt'};
+vIn = sumblk('y=yt+v');
+
+kalmf.InputName = {'u','y'};
+kalmf.OutputName = 'ye';
+
+SimModel = connect(sys,vIn,kalmf,{'u','w','v'},{'yt','ye'});
+
+t = (0:100)';
+u = sin(t/5);
+
+rng(10,'twister');
+w = sqrt(Q)*randn(length(t),1);
+v = sqrt(R)*randn(length(t),1);
+
+out = lsim(SimModel,[u,w,v]);
+
+yt = out(:,1);   % true response
+ye = out(:,2);  % filtered response
+y = yt + v;     % measured response
+
+
+clf
+subplot(211), plot(t,yt,'b',t,ye,'r--'), 
+xlabel('Number of Samples'), ylabel('Output')
+title('Kalman Filter Response')
+legend('True','Filtered')
+subplot(212), plot(t,yt-y,'g',t,yt-ye,'r--'),
+xlabel('Number of Samples'), ylabel('Error')
+legend('True - measured','True - filtered')
+
+
+
+
+
+
+%%
+
 Plant = ss(A,B,C,D,-1);
 Plant.InputName = 'un';
 Plant.OutputName = 'yt';
