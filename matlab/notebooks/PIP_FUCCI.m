5d2f6823da6ea1c662d48c6992ed78d8e7719993
JPickard1 <jpic@umich.edu>
2023-10-02 11:51:27 -0400

    2015 + 2018 figure set up

--- matlab/notebooks/PIP_FUCCI.m
+++ matlab/notebooks/PIP_FUCCI.m
@@ -13,7 +13,8 @@
 %% Load data
 clear; close all; clc;
 cellCycleGenes = readtable('data/kegg_hsa04110.csv');   % Load cell cycle genes
-[D,G] = loadMYOD(); replicates = 3;                     % Load data set
+% [D,G,replicates] = loadMYOD(); % Load data set
+[D,G,replicates] = load2015(); % Load data set
 gene2idx = containers.Map;                              % Map gene names to indices
 for i=1:numel(G); gene2idx(string(G{i})) = i; end
 cellCycleIdxs = [];                                     % Identify cell cycle indices from the data
@@ -40,7 +41,7 @@ for i=1:height(T)
     ensg2name(e) = n;
 end
 %}
-HG = HWG.geneIndexTable;                   % populate HWG table with more gene names                    
+HG = HWG.geneIndexTable;                   % populate HWG table with more gene names
 hgname2idx = containers.Map;
 for i=1:height(HG)
     if ~isempty(HG.("Gene Name"){i})
@@ -301,6 +302,160 @@ figure; P = plot(graph(A)); P.NodeLabel = GC;
 
 %% Perform DMD
 dmd = shiftedDMD(DC,3,[],0.9);
+
+%% One figure with all the types of data
+clear; close all; clc;
+
+thresh=0.999; % DMD threshold
+
+load('C:\Users\picka\Documents\my_projects\DBTM\HardwiredGenome\Data\Processed\HWG\000\HWG.mat');
+HG = HWG.geneIndexTable;                   % populate HWG table with more gene names                    
+name2idx = containers.Map;                % build map so that HWG table may be populated faster
+for i=1:height(HG)
+    n = string(HG.("Gene Name"){i});
+    if ~isempty(n)
+        name2idx(n) = i;
+    end
+end
+
+
+cellCycleGenes = readtable('data/kegg_hsa04110.csv');   % Load cell cycle genes
+
+fig = figure;
+set(fig, 'Units', 'inches', 'Position', [1, 1, 13, 6]); % Adjust the figure size
+tiledlayout(2, 2);
+for ds=1:2
+    % Load particular data
+    if ds==1
+        [D,G,replicates] = loadMYOD(); % Load data set
+    elseif ds == 2
+        [D,G,replicates] = load2015(); % Load data set
+    end
+
+    gene2idx = containers.Map;                              % Map gene names to indices
+    for i=1:numel(G); gene2idx(string(G{i})) = i; end
+    cellCycleIdxs = [];                                     % Identify cell cycle indices from the data
+    for i=1:height(cellCycleGenes)
+        if gene2idx.isKey(string(cellCycleGenes.GeneName{i}))
+            cellCycleIdxs(end+1) = gene2idx(string(cellCycleGenes.GeneName{i}));
+        end
+    end
+    
+    GC = G(cellCycleIdxs);      % Extract cell cycle gene indices
+    DC = D(cellCycleIdxs,:);    % Extract cell cycle RNAseq
+
+    sensors = ["GEM", "PCNA", "CDT1"];
+    sensorIdxs = [];
+    for s=1:length(sensors)
+        for i=1:length(GC)
+            if strcmp(sensors(s), string(GC{i}))
+                sensorIdxs(end+1) = i;
+                break;
+            end
+        end
+    end
+    
+    % Identify largest observable component
+    % perform DMD to construct A matrix
+    dmd = shiftedDMD(DC,3,[],thresh);
+    A = dmd.Xp*dmd.DMD.VX*pinv(dmd.DMD.Sig)*dmd.DMD.UX';
+        
+    % Construct GRN from DMD A
+    threshW = 0.25;
+    Ag = (A > threshW) + (A < -1 * threshW); Ag = Ag + Ag';
+    g = graph(Ag);
+    
+    % Rank all genes according to their distances from CDT1, PCNA, and GEM
+    d = distances(g);
+    dists = zeros(size(DC,1), 3);
+    for s=1:3
+        for i=1:size(DC,1)
+            dists(i,s) = d(i,sensorIdxs(s));
+        end
+    end
+    totalDist = sum(dists,2);
+    [~, idx] = sort(totalDist,'ascend');
+    
+    % while the system is fully observable, add new genes until it is not fully
+    % observable
+    reducedSystem = sensorIdxs;
+    reducedSensors = [1 2 3];
+    r = 3;
+    i = 1;
+    while true
+        DR = DC(reducedSystem,:);
+        dmd = shiftedDMD(DR,3,[],thresh);
+        Ar = dmd.Xp*dmd.DMD.VX*pinv(dmd.DMD.Sig)*dmd.DMD.UX';
+        Cr = getC(reducedSensors, size(Ar,1));
+        Or = obsv(Ar,Cr);
+        r = rank(Or);
+        if length(reducedSystem) == r
+            % add a new vertex
+            while find(reducedSystem == idx(i))
+                i = i + 1;
+            end
+            reducedSystem = [reducedSystem idx(i)];
+        else
+            reducedSystem(end) = [];
+            break
+        end
+    end
+    
+    % Plot the reduced system
+    DR = DC(reducedSystem,:);
+    dmd = shiftedDMD(DR,3,[],thresh);
+    A = dmd.Xp*dmd.DMD.VX*pinv(dmd.DMD.Sig)*dmd.DMD.UX';
+    GR = GC(reducedSystem);
+    
+    % Create a heatmap
+    nexttile;
+    imagesc(A);
+    colormap('jet');
+    colorbar;
+    % set(gca, 'XTick', [], 'YTick', 1:34, 'YTickLabel', GR);
+    set(gca, 'XTick', 1:34, 'XTickLabel', GR, 'YTick', 1:34, 'YTickLabel', GR);
+    xtickangle(60); % Rotate X-axis labels for readability
+    title('Linearized Dynamics (DMD)', 'Interpreter', 'latex', 'FontSize', 14); % Use LaTeX for title
+    
+    % Construct GRN from DMD A
+    nexttile;
+    threshW = 2;
+    Ag = (A > threshW) + (A < -1 * threshW); Ag = Ag + Ag';
+    g = graph(Ag);
+    
+    TFs = HG.("Transcription Factor");
+    GRtf = zeros(length(GR),1);
+    for i=1:length(GR)
+        idx = name2idx(GR{i});
+        GRtf(i) = TFs(idx);
+    end
+
+    % Define which nodes to label and their categories
+    nodes_to_label = [1, 2, 3, find(GRtf == 1)'];
+    node_categories = {'Gene', 'Sensor', 'Transcription Factor'}; % Assign categories to nodes
+    node_colors = {'g', 'g', 'g', 'r', 'r'}; % Assign colors to categories
+
+    P = plot(g, 'NodeLabel', {}, 'MarkerSize', 10, 'LineWidth', 1.2); % Customize node appearance
+    % P.NodeLabel = {}; % Remove node labels for all nodes
+    P.NodeLabel = GR;
+    
+    % Label nodes with categories and colors
+    for i=1:length(GR)
+        if find(nodes_to_label == i)
+            if i <= 3; color = 'g';
+            else; color = 'r'; end
+            highlight(P, i, 'NodeColor', color);
+        else
+            disp(GR(i))
+            % label = "X";
+            labelnode(P,[i],"")
+            % P.NodeLabel{i} = label;
+        end
+    end
+    title('Observable Cell Cycle Network', 'Interpreter', 'latex', 'FontSize', 14); % Use LaTeX for title
+end
+sgtitle('PIP-FUCCI Observability', 'Interpreter', 'latex', 'FontSize', 16); % Use LaTeX for super title
+% clearvars -except GC DC replicates
 
 
 
