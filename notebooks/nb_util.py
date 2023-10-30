a85ff9142d74c5f3e4d33b2b8d51a250e80d1ecb
CooperStansbury <cstansbu@umich.edu>
2023-10-30 15:57:32 -0400

    updater TPM thresholding

--- notebooks/nb_util.py
+++ notebooks/nb_util.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import sys
+from sklearn.preprocessing import StandardScaler
 
 tmap = {
     'S1a' : 0,
@@ -66,6 +67,42 @@ cmap = {
     'S9b' : "timecourse",
 }
 
+
+s_genes = [
+    'MCM5','PCNA','TYMS','FEN1','MCM7','MCM4',
+    'RRM1','UNG','GINS2','MCM6','CDCA7','DTL','PRIM1',
+    'UHRF1','CENPU','HELLS','RFC2','POLR1B','NASP',
+    'RAD51AP1','GMNN','WDR76','SLBP','CCNE2','UBR7',
+    'POLD3','MSH2','ATAD2','RAD51','RRM2','CDC45',
+    'CDC6','EXO1','TIPIN','DSCC1','BLM','CASP8AP2',
+    'USP1','CLSPN','POLA1','CHAF1B','MRPL36','E2F8'
+]
+
+g2_genes = [
+    'HMGB2','CDK1','NUSAP1','UBE2C','BIRC5',
+    'TPX2','TOP2A','NDC80','CKS2','NUF2',
+    'CKS1B','MKI67','TMPO','CENPF','TACC3',
+    'PIMREG','SMC4','CCNB2','CKAP2L','CKAP2',
+    'AURKB','BUB1','KIF11','ANP32E','TUBB4B',
+    'GTSE1','KIF20B','HJURP','CDCA3','JPT1',
+    'CDC20','TTK','CDC25C','KIF2C','RANGAP1',
+    'NCAPD2','DLGAP5','CDCA2','CDCA8','ECT2',
+    'KIF23','HMMR','AURKA','PSRC1','ANLN',
+    'LBR','CKAP5','CENPE','CTCF','NEK2',
+    'G2E3','GAS2L3','CBX5','CENPA'
+]
+
+def meltDf(df):
+    """A custom function to transform the raw data
+    representation into a melted one """
+    df = pd.melt(df, id_vars='geneName')
+    df['timepoint'] = df['variable'].map(tmap)
+    df['replicate'] = df['variable'].map(rmap)
+    df['control'] = df['variable'].map(cmap)
+    df['hours'] = df['timepoint'] * 8
+    return df
+    
+
 def getGeneLengths(gene_table_path, gene_names):
     """A function to get gene lengths from a gene_table file
 
@@ -111,3 +148,51 @@ def CPM(df, target=1e6):
     for c in cpm.columns:
         cpm[c] = (df[c] / df[c].sum()) *  target
     return cpm
+
+
+def getFCFrame(df, cols):
+    """A function to return a new dataframe with 
+    foldchanges over the initial. Expects cols to be time-ordered
+    and that cols[0] is the initial condition """
+    df2 = df[cols].copy()
+    x0 = cols[0] # initial condition
+    for c in cols[1:]:
+        df2[c] = (df2[c] + 1) /  (df2[x0].mean() + 1)
+    return df2[cols[1:]].copy()
+
+
+def data2DMD(df):
+    """A function to make DMD suitable data from the 2015 data"""
+    a = [x for x in df.columns if "a" in x]
+    b = [x for x in df.columns if "b" in x]
+
+    dfa = getFCFrame(df, a)
+    dfb = getFCFrame(df, b)
+
+    scaler = StandardScaler()
+
+    dfa = scaler.fit_transform(dfa)
+    dfb = scaler.fit_transform(dfb)
+    
+    dmd_data = np.asarray([dfa, dfb])
+    dmd_data = np.swapaxes(dmd_data, 0, 2)
+    dmd_data = np.swapaxes(dmd_data, 0, 1)
+
+    return dmd_data
+
+
+def getMuData(df):
+    """A function to get the mean expression of each replicate at
+    each time point """
+    mu_data = df.copy()
+    mu_data = mu_data.T
+
+    mu_data = mu_data.reset_index(drop=False)
+    mu_data['time'] = mu_data['index'].apply(lambda x: x.replace("a", "").replace("b", "").replace('S', ''))
+    mu_data['time'] = mu_data['time'].astype(int)
+    mu_data = mu_data.drop(columns=['index'])
+    mu_data = mu_data.groupby('time').mean().reset_index(drop=False)
+    mu_data = mu_data.sort_values(by='time')
+    mu_data = mu_data.set_index('time')
+    return mu_data
+    
